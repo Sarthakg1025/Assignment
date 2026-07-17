43fc02bcf4ffa2ce341e51068c465fc8b69a20d5
Sarthak  Ghodekar <sarthakghodekar64@gmail.com>
2026-07-18 04:16:49 +0530

    Add files via upload

--- Assignment 26/Ass26q1_OOP.py
+++ Assignment 26/Ass26q1_OOP.py
@@ -1,1 +1,22 @@
+class Demo:
+	value=0
 
+	def __init__ (self,No1,No2):
+		self.No1=No1
+		self.No2=No2
+
+	def Fun(self):
+		print(self.No1)
+		print(self.No2)
+
+	def Gun(self):
+		print(self.No1)
+		print(self.No2)
+
+dobj1=Demo(11,21)
+dobj2=Demo(51,101)
+
+dobj1.Fun()
+dobj2.Fun()
+dobj1.Gun()
+dobj2.Gun()	
